aaf50befa533299f1647d0702e245960badbcc57
jensbrauer <henningsson.jens@gmail.com>
2022-12-15 22:13:24 +0000

    Added computers gamearea to anounce computers chosen weapon and display a button for next round

--- assets/js/rps.js
+++ assets/js/rps.js
@@ -1,14 +1,17 @@
 const userScoreHtml = document.getElementById('rounds-won')
 const computerScoreHtml = document.getElementById('rounds-lost')
+const rockButton = document.getElementById('rock-button')
+const paperButton = document.getElementById('paper-button')
+const scissorsButton = document.getElementById('scissors-button')
+const nextRoundButton = document.getElementById('computer-right')
 
 function runRound(userInput) {
+    nextRoundButton.style.visibility = 'visible';
     //returns string of 'rock', 'paper' or 'scissors'
     let computerInput = generateComputerInput()
     //returns string of 'win', 'draw' or 'loose'
     let roundResult = getRoundResult(userInput, computerInput)
-    //returns true if point awarded for the round gives player or computer three point and win the game
-    console.log(isGameOver(roundResult))
-    //Choose an appropriate respons-function based on state
+    //Choose an appropriate respons-function based on state of isGameOver and roundResult
     if(isGameOver(roundResult)) {
         if (roundResult == 'win'){
             gameOverByWin(userInput, computerInput)
@@ -77,19 +80,40 @@ function isGameOver(roundResult) {
 
 //Respons functions to update html
 function gameOverByWin(userInput, computerInput){
+    colorButton(userInput)
     userScoreHtml.textContent = Number(userScoreHtml.textContent) + 1
 
 }
 function gameOverByLoose(userInput, computerInput){
+    colorButton(userInput)
     computerScoreHtml.textContent = Number(computerScoreHtml.textContent) + 1
 
 }
 function userWonRound(userInput, computerInput){
+    colorButton(userInput)
     userScoreHtml.textContent = Number(userScoreHtml.textContent) + 1
 }
 function computerWonRound(userInput, computerInput){
+    colorButton(userInput)
     computerScoreHtml.textContent = Number(computerScoreHtml.textContent) + 1
 }
 function roundDraw(userInput, computerInput){
+    colorButton(userInput)
+}
+function startNewGame() {
+    userScoreHtml.textContent = 0
+    computerScoreHtml.textContent = 0
 
+    //let container = document.getElementById('hej')
+    //container.innerHTML = `<i class="fa-solid fa-hand-fist"></i>`
+
+}
+function colorButton(userInput){
+    if (userInput == 'rock'){
+        rockButton.style.backgroundColor = "darkslategray"
+    } else if (userInput == 'paper'){
+        paperButton.style.backgroundColor = "darkslategray"
+    } else {
+        scissorsButton.style.backgroundColor = "darkslategray"
+    }
 }
