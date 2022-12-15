1f83cdb1a1bb0f533789dc08a3159fbd41498c27
jensbrauer <henningsson.jens@gmail.com>
2022-12-15 18:55:31 +0000

    Added function to determine win loose or draw agains generated computer input.

--- assets/js/rps.js
+++ assets/js/rps.js
@@ -1,3 +1,46 @@
 function runRound(userInput) {
     console.log(userInput)
+    let computer = generateComputerinput()
+    console.log(computer)
+    console.log(didWin(userInput, computer))
+}
+
+function generateComputerinput() {
+    let calc = Math.floor(Math.random() * (3) );
+    if (calc == 0){
+        return 'rock'
+    } else if(calc == 1) {
+        return 'paper'
+    } else {
+        return 'scissors'
+    }
+
+}
+
+function didWin(user, computer){
+    if (user == 'rock'){
+        if (computer == 'rock'){
+            return 'draw'
+        } else if (computer == 'paper'){
+            return 'loose'
+        } else {
+            return 'win'
+        }
+    } else if (user == 'paper'){
+        if (computer == 'rock'){
+            return 'win'
+        } else if (computer == 'paper'){
+            return 'draw'
+        } else {
+            return 'loose'
+        }
+    } else {
+        if (computer == 'rock'){
+            return 'loose'
+        } else if (computer == 'paper'){
+            return 'win'
+        } else {
+            return 'draw'
+        }
+    }
 }
